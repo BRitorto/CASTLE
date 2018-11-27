70954939c5902c7e1b4e03fca5ed9c7ff99163f6
clara guzzetti <cguzzetti@itba.edu.ar>
2018-11-27 00:04:42 -0300

    abstract syntax tree

---
+++ node.c
@@ -0,0 +1,174 @@
+//credit to jefa lel
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "node.h"
+#include <string.h>
+
+static Node *
+new_node(token token, char * value){
+	Node * a = (Node *)malloc(sizeof(*a));
+	a->token = token;
+	a->value = value;
+	a->index = 0;
+	a->children = malloc(10 * sizeof(*a->children));
+	return a;
+}
+
+void
+add_node(Node * p, Node * c){
+	p->children[p->index++] = c;
+}
+
+Node *
+new_tree(){
+	return new_node(root_, NULL);
+}
+
+void
+add_terminal_node(Node * p, token token){
+	if(p == NULL)
+		return;
+
+	p->children[p->index++] = new_node(token, NULL);
+}
+
+void
+set_value(Node * n, char * new_value){
+	n->value = new_value;
+}
+
+char *
+get_value(Node * n){
+	return n->value;
+}
+
+static int random_names = 1;
+
+
+void
+print_tree(Node * t){
+
+	if(t->index == 0){
+
+		set_info(t);
+
+		if(t->value == NULL){
+			printf("%s\n", "MALO");
+		}else{
+			printf("%s",t->value );
+		}
+
+		return;
+
+	}
+
+
+	switch(t->token){
+
+		case and_:
+
+			printf("%s", "BasicOperations.intersection(");
+			print_tree(t->children[0]);
+			putchar(',');
+			print_tree(t->children[2]);
+			printf("%s", ")");
+
+		break;
+
+		case or_:
+
+			printf("%s", "BasicOperations.union(");
+			print_tree(t->children[0]);
+			putchar(',');
+			print_tree(t->children[2]);
+			printf("%s", ")");
+
+		break;
+
+		case ne_:
+			putchar('!');
+
+			//no pongo break !!
+
+		case equalscmp_:
+			print_tree(t->children[0]);
+			printf("%s",".equals(");
+			print_tree(t->children[2]);
+			printf("%s", ")");
+
+		break;
+
+
+		case print_:
+			printf("%s","System.out.println(" );
+			print_tree(t->children[1]);
+			printf("%s", ")");
+			break;
+
+		case scan_:
+
+			print_tree(t->children[1]);
+			printf("%s", "= scan.nextLine()");
+
+		break;
+
+
+		default:
+		{
+			for(int i = 0; i < t->index; i++){
+				print_tree(t->children[i]);
+			}
+		};
+	}
+
+}
+
+void
+print_program(Node * t){
+	print_headers();
+	print_tree(t);
+	print_end();
+}
+
+void
+print_headers() {
+	char * headers = "import java.util.Scanner; public class Main { public static void main(String[] args) {";
+	printf("%s",headers);
+	printf("%s\n","Scanner scan = new Scanner(System.in);");
+}
+
+void
+print_end() {
+	printf("%s\n", "scan.close();");
+	printf("%s\n", "}}");
+}
+
+void
+set_info(Node * t) {
+
+	if(t->token == int_) {
+
+		char * text = "new Integer(";
+		char * aux = t->value;
+		t->value = malloc(strlen(text) + strlen(t->value) + 3);
+		sprintf(t->value, "new Integer(%s)", aux);
+	}
+	if(t->token == int_ || t->token == string_ || t->token == id_) {
+		return;
+	}
+
+
+
+	t->value = tokens[t->token];
+}
+
+void
+add_terminal_node_with_value(Node * p, token token, char * value) {
+
+	if(p == NULL)
+		return;
+
+	p->children[p->index++] = new_node(token, value);
+
+}
---
+++ node.h
@@ -0,0 +1,51 @@
+//credit to jefa lel
+
+typedef enum token {
+
+		while_, lparen_, rparen_, lbrack_, rbrack_, if_, else_, end_line_, equal_, or_, and_, equalscmp_, diff_, lthan_, lethan_, gthan_, gethan_, plus_, minus_, mult_, div_, string_,  int_, new_,  print_,  true_, false_,int_, string_,  scan_,
+
+    //dummy
+    root_,
+
+    // Non terminals
+    file_, statement_, definition_, assignment_,  type_, expression_, conjunction_, equality_, equop_, relation_, relop_, addition_,   addop_, term_, mulop_, factor_, unaryop_, primary_, fuint_, fustring_, reint_, restring_, operand_, boperator,
+
+} token;
+
+static char * tokens[255] = {"while", "(", ")", "{\n", "}\n", "if", "else", ";\n", "=", "||", "&&", ".equals(", "!", "<", "<=", ">", ">=", "+", "-", "*", "/", "String ", "Integer ", "new ", "syso", "(new Integer(1).equals(1)) ", "(new Integer(1).equals(0))"};
+
+typedef struct Node_t {
+	struct Node_t ** children;
+	char * value;
+	int index;
+	token token;
+} Node;
+
+Node *
+new_tree();
+
+void
+add_terminal_node(Node * p, token token);
+
+void
+set_info(Node * n);
+
+char *
+get_info(Node * n);
+void
+add_node(Node * p, Node * c);
+
+void
+print_tree(Node * t);
+
+void
+add_terminal_node_with_value(Node * p, token token, char * value);
+
+void
+print_headers();
+
+void
+print_program();
+
+void
+print_end();
